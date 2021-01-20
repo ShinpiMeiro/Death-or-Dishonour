f1e695bb250ad5ebcbf8ed4ab2224e881b53f4c1
shin <max.rozoff@gmail.com>
2021-01-20 21:17:38 +0300

    player_class update

--- player_class.py
+++ player_class.py
@@ -6,7 +6,9 @@ class Player:
     def __init__(self):
         self.moving_left = False
         self.moving_right = False
+        self.pos = (200, 600)
         self.hp = 3
         # Регистрация метода handle() ракетки для обработки событий клавиш
+
     def draw(self, surface):
-        pygame.draw.rect(surface, (110, 150, 110), self.bounds)
+        pygame.draw.rect(surface, (110, 150, 110), [self.pos[0], self.pos[1], 40, 10])
